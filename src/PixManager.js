4768342a9b792ea619eff48d0873308294955a85
andreilupu <andrei.lupu@pixelgrade.com>
2017-03-21 00:32:49 +0200

    play with wp cli resets

--- src/PixManager.js
+++ src/PixManager.js
@@ -15,17 +15,22 @@ module.exports = function ( context ) {
 			this.state = {
 				content: null,
 				active_plugins: null,
-				inactive_plugins: null
+				inactive_plugins: null,
+				active_theme: null,
 			}
 
-			this.click_on_reset = this.click_on_reset.bind(this);
+			this.wp_cli_reset_db = this.wp_cli_reset_db.bind(this);
+			this.wp_cli_remove_theme_mods = this.wp_cli_remove_theme_mods.bind(this);
+			this.wp_cli_remove_transients = this.wp_cli_remove_transients.bind(this);
 		}
 
 		componentDidMount() {
 			// set up 
 			if ( 'running' === this.props.siteStatus ) {
 				// console.log( this );
-				// this.getPluginList();
+				this.getPluginList();
+				this.getThemeLists();
+				this.getThemeTools();
 			} else {
 				this.setState( { content: <p>Machine not running!</p> } )
 			}
@@ -33,6 +38,59 @@ module.exports = function ( context ) {
 
 		componentWillUnmount() {
 			// tear down
+		}
+
+		getThemeTools(){
+
+			let site = this.props.sites[ this.props.params.siteID ];
+			let path = '"' + context.environment.dockerPath  +'"';
+
+			// construct command using bundled docker binary to execute 'wp plugin list' inside container
+			let tessst = `${path} exec ${site.container} "cd ./app; ls"`
+
+			console.log(context.environment);
+			console.log(site);
+
+			// execute command in docker env and run callback when it returns
+			childProcess.exec( tessst, { env: context.environment.dockerEnv }, (error, stdout, stderr) => {
+				// Display error message if there's an issue
+				if (error) {
+
+					console.log(error);
+				} else {
+					console.debug(stdout);
+
+				}
+			} );
+		}
+
+		getThemeLists() {
+
+			// get site object using siteID
+			let site = this.props.sites[ this.props.params.siteID ]
+			let path = '"' + context.environment.dockerPath  +'"'
+			// console.log();
+
+			// construct command using bundled docker binary to execute 'wp plugin list' inside container
+			let wp_cli_get_themes = `${path} exec ${site.container} wp option get template --allow-root`
+
+			// execute command in docker env and run callback when it returns
+			childProcess.exec( wp_cli_get_themes, { env: context.environment.dockerEnv }, (error, stdout, stderr) => {
+				// Display error message if there's an issue
+				if (error) {
+					this.setState( { active_theme:  <p>Error retrieving active theme list: <pre>{stderr}</pre></p> } )
+				} else {
+					console.debug(stdout);
+					// split list into array
+					let theme = stdout.trim().split( "\n" )
+					// Only create unordered list if there are plugins to list
+					if ( theme.length && theme[0].length > 0 ) {
+						this.setState( { active_theme: theme} );
+					} else {
+						this.setState( { active_theme: <p>No active theme.</p> } );
+					}
+				}
+			} );
 		}
 
 		getPluginList() {
@@ -51,17 +109,16 @@ module.exports = function ( context ) {
 			childProcess.exec( wp_cli_get_active_plugins, { env: context.environment.dockerEnv }, (error, stdout, stderr) => {
 				// Display error message if there's an issue
 				if (error) {
-					this.setState( { content:  <p>Error retrieving active plugin list: <pre>{stderr}</pre></p> } )
+					this.setState( { active_plugins:  <p>Error retrieving active plugin list: <pre>{stderr}</pre></p> } )
 				} else {
 					// split list into array
 					let plugins = stdout.trim().split( "\n" )
 					// Only create unordered list if there are plugins to list
 					if ( plugins.length && plugins[0].length > 1 ) {
 						{/*this.setState( { content: <ul>{ plugins.map( (item) => <li key={ plugins.indexOf(item) }>{ item }</li> ) }</ul> } );*/}
-
 						this.setState( { active_plugins: plugins} );
 					} else {
-						this.setState( { content: <p>No active plugins.</p> } )
+						this.setState( { active_plugins: <p>No active plugins.</p> } );
 					}
 				}
 			} );
@@ -78,10 +135,10 @@ module.exports = function ( context ) {
 					// Only create unordered list if there are plugins to list
 					if ( plugins.length && plugins[0].length > 1 ) {
 						// this.setState( { content: <ul>{ plugins.map( (item) => <li key={ plugins.indexOf(item) }>{ item }</li> ) }</ul> } );
-						this.setState( { content: stdout.trim() } );
+						// this.setState( { inactive_plugins: stdout.trim() } );
 						this.setState( { inactive_plugins: plugins} );
 					} else {
-						this.setState( { content: <p>No inactive plugins.</p> } )
+						this.setState( { inactive_plugins: <p>No inactive plugins.</p> } )
 					}
 				}
 			} );
@@ -103,12 +160,19 @@ module.exports = function ( context ) {
 
 					<hr style={{ padding: '0 20px', flexBasis: '100%'}} />
 
-					<div id="tools" style={{ padding: '0 2px', flexBasis: '100%'}}>
+					<div id="tools" style={halfStyle}>
 						<h3>Database tools</h3>
 						<ul>
-							<li>
-								<button className="btn btn-flat" id="db_reset" style={{ color: '#E3004A' }} onClick={this.click_on_reset}>Empty this site!</button>
-							</li>
+							<li><button className="btn btn-flat" id="db_reset" style={{ color: '#E3004A' }} onClick={this.wp_cli_reset_db}>Empty this site!</button></li>
+							<li><button className="btn btn-flat" id="db_reset" style={{ color: '#E3004A' }} onClick={this.wp_cli_remove_theme_mods}>Delete Theme Mods!</button></li>
+							<li><button className="btn btn-flat" id="db_reset" style={{ color: '#E3004A' }} onClick={this.wp_cli_remove_transients}>Delete Transients!</button></li>
+						</ul>
+					</div>
+
+					<div id="theme_tools" style={halfStyle}>
+						<h3>Active theme: {this.state.active_theme ? this.state.active_theme : 'loading ...'}</h3>
+						<ul>
+							<li><button className="btn btn-flat" id="active_t" style={{ color: '#E3004A' }} onClick={this.wp_cli_reset_db}>Empty this site!</button></li>
 						</ul>
 					</div>
 
@@ -163,11 +227,10 @@ module.exports = function ( context ) {
 			</div>);
 		}
 
-		click_on_reset () {
+		wp_cli_reset_db () {
 			var confirm = window.confirm('Are you sure?');
 
 			if ( confirm ) {
-
 				let path = '"' + context.environment.dockerPath  +'"',
 					site = this.props.sites[ this.props.params.siteID ], // get site object using siteID
 					reset_command = `${path} exec ${site.container} wp db reset --yes --allow-root`;
@@ -177,6 +240,60 @@ module.exports = function ( context ) {
 					// Display error message if there's an issue
 					if (error) {
 						this.setState( { content:  <p>Error on reset: <pre>{stderr}</pre></p> } )
+					} else {
+						// split list into array
+						let result = stdout.trim();
+						// Only create unordered list if there are plugins to list
+						// if ( result.indexOf('Succes') !== -1 ) {
+							this.setState( { content: <p>{result}</p> } )
+						// }
+					}
+				} );
+			} else {
+				console.log(this);
+			}
+		}
+
+		wp_cli_remove_transients () {
+			var confirm = window.confirm('Are you sure?');
+
+			if ( confirm ) {
+				let path = '"' + context.environment.dockerPath  +'"',
+					site = this.props.sites[ this.props.params.siteID ], // get site object using siteID
+					reset_command = `${path} exec ${site.container} wp transient delete --all --allow-root`;
+
+				// execute command in docker env and run callback when it returns
+				childProcess.exec( reset_command, { env: context.environment.dockerEnv }, (error, stdout, stderr) => {
+					// Display error message if there's an issue
+					if (error) {
+						this.setState( { content:  <p>Error, cannot delete transients: <pre>{stderr}</pre></p> } )
+					} else {
+						// split list into array
+						let result = stdout.trim();
+						// Only create unordered list if there are plugins to list
+						// if ( result.indexOf('Succes') !== -1 ) {
+							this.setState( { content: <p>{result}</p> } )
+						// }
+					}
+				} );
+			} else {
+				console.log(this);
+			}
+		}
+
+		wp_cli_remove_theme_mods () {
+			var confirm = window.confirm('Are you sure?');
+
+			if ( confirm ) {
+				let path = '"' + context.environment.dockerPath  +'"',
+					site = this.props.sites[ this.props.params.siteID ], // get site object using siteID
+					reset_command = `${path} exec ${site.container} wp theme mod remove --all --allow-root`;
+
+				// execute command in docker env and run callback when it returns
+				childProcess.exec( reset_command, { env: context.environment.dockerEnv }, (error, stdout, stderr) => {
+					// Display error message if there's an issue
+					if (error) {
+						this.setState( { content:  <p>Error, Cannot remove theme mods: <pre>{stderr}</pre></p> } )
 					} else {
 						// split list into array
 						let result = stdout.trim();
