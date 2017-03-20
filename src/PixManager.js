bc032081f876b335c4f3dcba26a9512e5ee93acb
andreilupu <andrei.lupu@pixelgrade.com>
2017-03-20 12:22:17 +0200

    play with a db reset button

--- src/PixManager.js
+++ src/PixManager.js
@@ -1,5 +1,6 @@
 const childProcess = require ('child_process' )
 // import Plugins from 'Plugins'
+const fs = require('fs');
 
 module.exports = function ( context ) {
 
@@ -10,20 +11,20 @@ module.exports = function ( context ) {
 	return class SitePixManager extends Component {
 		constructor( props ) {
 			super( props )
-
-
 			
 			this.state = {
 				content: null,
 				active_plugins: null,
 				inactive_plugins: null
 			}
+
+			this.click_on_reset = this.click_on_reset.bind(this);
 		}
 
 		componentDidMount() {
 			// set up 
 			if ( 'running' === this.props.siteStatus ) {
-				console.log( this );
+				// console.log( this );
 				// this.getPluginList();
 			} else {
 				this.setState( { content: <p>Machine not running!</p> } )
@@ -39,13 +40,12 @@ module.exports = function ( context ) {
 				
 			// get site object using siteID
 			let site = this.props.sites[ this.props.params.siteID ]
-
 			let path = '"' + context.environment.dockerPath  +'"'
 			// console.log();
 
 			// construct command using bundled docker binary to execute 'wp plugin list' inside container
-			let wp_cli_get_active_plugins = `${path} exec ${site.container} wp plugin list --path=/app/public --field=name --status=active --allow-root`
-			let wp_cli_get_inactive_plugins = `${path} exec ${site.container} wp plugin list --path=/app/public --field=name --status=inactive --allow-root`
+			let wp_cli_get_active_plugins = `${path} exec ${site.container} wp plugin list --field=name --status=active --allow-root`
+			let wp_cli_get_inactive_plugins = `${path} exec ${site.container} wp plugin list --field=name --status=inactive --allow-root`
 
 			// execute command in docker env and run callback when it returns
 			childProcess.exec( wp_cli_get_active_plugins, { env: context.environment.dockerEnv }, (error, stdout, stderr) => {
@@ -103,6 +103,17 @@ module.exports = function ( context ) {
 
 					<hr style={{ padding: '0 20px', flexBasis: '100%'}} />
 
+					<div id="tools" style={{ padding: '0 2px', flexBasis: '100%'}}>
+						<h3>Database tools</h3>
+						<ul>
+							<li>
+								<button className="btn btn-flat" id="db_reset" style={{ color: '#E3004A' }} onClick={this.click_on_reset}>Empty this site!</button>
+							</li>
+						</ul>
+					</div>
+
+					<hr style={{ padding: '0 20px', flexBasis: '100%'}} />
+
 					<div style={halfStyle}>
 						<h3>Active Plugins</h3>
 						<div>
@@ -150,6 +161,34 @@ module.exports = function ( context ) {
 			}
 
 			</div>);
+		}
+
+		click_on_reset () {
+			var confirm = window.confirm('Are you sure?');
+
+			if ( confirm ) {
+
+				let path = '"' + context.environment.dockerPath  +'"',
+					site = this.props.sites[ this.props.params.siteID ], // get site object using siteID
+					reset_command = `${path} exec ${site.container} wp db reset --yes --allow-root`;
+
+				// execute command in docker env and run callback when it returns
+				childProcess.exec( reset_command, { env: context.environment.dockerEnv }, (error, stdout, stderr) => {
+					// Display error message if there's an issue
+					if (error) {
+						this.setState( { content:  <p>Error on reset: <pre>{stderr}</pre></p> } )
+					} else {
+						// split list into array
+						let result = stdout.trim();
+						// Only create unordered list if there are plugins to list
+						// if ( result.indexOf('Succes') !== -1 ) {
+							this.setState( { content: <p>{result}</p> } )
+						// }
+					}
+				} );
+			} else {
+				console.log(this);
+			}
 		}
 	}
 
